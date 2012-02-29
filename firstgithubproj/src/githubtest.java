43d5089fb4026ed7d17d7deb103820249e3f2ce1
Anna <annagala@hotmail.com>
2012-02-28 23:58:57 -0800

    commiting

--- firstgithubproj/src/githubtest.java
+++ firstgithubproj/src/githubtest.java
@@ -1,5 +1,6 @@
 public class githubtest {
 	public static void main(String[] args) {
 		System.out.println("Git is fun 2");
+		//
 	}
 }
--- firsttest/src/GitTest.java
+++ firsttest/src/GitTest.java
@@ -2,5 +2,7 @@
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
+		
+		//new comment
 	}
 }
